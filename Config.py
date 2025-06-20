69e9a005aa7742d791182594fed6c18d555cdba4
Bankrats001 <njerindungu066@gmail.com>
2025-06-20 13:43:49 +0300

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,17 @@
+# config.py
+
+# Your Telegram Bot Token (from BotFather)
+API_TOKEN = "7808962933:AAEvQ_KG0Au24RK_Btez7rMvI_n1ozwWW9A"
+
+# Your Telegram User ID (you as the BOT OWNER)
+# You can get it using @userinfobot or similar
+ADMIN_USER_ID = 7200774078  # <--- replace with your Telegram numeric ID!!
+
+# Your Binance email for payments
+BINANCE_EMAIL = "migosblazer4@gmail.com"
+
+# Your M-Pesa number for payments
+MPESA_NUMBER = "+254746362427"
+
+# USD → Ksh conversion rate (example: 1 USD = 130 Ksh)
+USD_KSH_RATE = 130.00
